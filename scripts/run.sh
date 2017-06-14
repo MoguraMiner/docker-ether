f327cac8e0b83922112b4f261d9a2faf202637d5
Aki Gao <xag9bb@virginia.edu>
2017-06-14 19:54:13 -0400

    update run script to clean up any terminated container named geth and runs the geth container a geth

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+echo cleaning up any leftover containers...
+bash ./clean.sh
+echo clean up complete!
+echo running container as geth...
 docker run -p 8545:8545 -p 30303:30303 --name geth mogura-geth
